8bc53869436d4c112e220128724f0b6e5432d997
alex69 <62944812+077729359@users.noreply.github.com>
2024-12-24 19:29:16 +0200

    Add files via upload

--- csv_loader.py
+++ csv_loader.py
@@ -1,20 +1,26 @@
 import csv
-from models import Schedule
+from schedule import Schedule
+from collections import defaultdict
+
 
 def load_schedule_from_csv(filename: str):
     schedules = []
+    index = defaultdict(list)  
+
     with open(filename, newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')  
+        reader = csv.reader(csvfile, delimiter=';')
         for row in reader:
             schedule = Schedule(
-                grupa=row[0],        
-                subject=row[1],      
-                teacher=row[2],    
-                start_time=row[3],  
-                end_time=row[4],     
-                week_type=row[5],   
-                building=row[6],    
-                day=row[7]           
+                grupa=row[0],
+                subject=row[1],
+                teacher=row[2],
+                start_time=row[3],
+                end_time=row[4],
+                week_type=row[5],
+                building=row[6],
+                day=row[7]
             )
             schedules.append(schedule)
-    return schedules
+            index[schedule.day].append(schedule)  
+
+    return schedules, index  
--- search.py
+++ search.py
@@ -1,11 +1,28 @@
-def search_schedules(schedules, group=None, subject=None, teacher=None, week_type=None, days=None):
+def search_schedules(index, schedules, group=None, subject=None, teacher=None, week_type=None, days=None):
     results = []
-    for schedule in schedules:
-        if (group and schedule.grupa not in group) or \
-           (subject and subject.lower() not in schedule.subject.lower()) or \
-           (teacher and teacher.lower() not in schedule.teacher.lower()) or \
-           (week_type and schedule.week_type not in week_type) or \
-           (days and schedule.day not in days):
-            continue
-        results.append(schedule)
+    
+    # daca avem zile de cautat
+    if days:
+        for day in days:
+            if day in index:  # verificam rapid în hash table
+                for schedule in index[day]:
+
+                    # verificam restul conditiilor
+                    if (group and schedule.grupa != group) or \
+                       (subject and subject.lower() not in schedule.subject.lower()) or \
+                       (teacher and teacher.lower() not in schedule.teacher.lower()) or \
+                       (week_type and schedule.week_type != week_type):
+                        continue
+                    results.append(schedule)
+    else:
+        # cautam dupa linear search daca nu avem zile in campul de cautare
+        
+        for schedule in schedules:
+            if (group and schedule.grupa != group) or \
+               (subject and subject.lower() not in schedule.subject.lower()) or \
+               (teacher and teacher.lower() not in schedule.teacher.lower()) or \
+               (week_type and schedule.week_type != week_type):
+                continue
+            results.append(schedule)
+    
     return results
